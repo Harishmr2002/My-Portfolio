6b8683b39a172da27401fd5c434e4acf6ae005b1
Harish M R <harishharshamr@gamil.com>
2025-07-22 16:10:07 +0530

    Setup and folder structure

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold text-blue-600">Hi, I’m Harish 👋</h1>
+      <p className="mt-2 text-lg text-gray-700">
+        Welcome to my React portfolio project.
+      </p>
+    </div>
+  );
+}
+
+export default App;
